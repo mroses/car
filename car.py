56931fdfb9c555d948d9b4cae886df25bb5d71ae
Marta <marta.rose@gmail.com>
2018-06-11 11:32:50 -0700

    finished car.py

--- car.py
+++ car.py
@@ -4,24 +4,25 @@ class Car(object):
         self.speed = speed
         self.fuel = fuel
         self. mileage = mileage
-        #self.tax = tax
-        
+        if price > 10000:
+            self.tax = .15
+        else:
+            self.tax = .12
+        self.display_all()
+
     def display_all(self):
-        print "Price: $" + str(self.price)       
+        print "Price: " + str(self.price) 
         print "Speed: " + str(self.speed)
         print "Fuel: " + str(self.fuel)
-        print "Mileage: " + string(self.mileage)
-        if price > 10000:
-                price = price * .15
-        else:
-            price = price * .12
-        return self
+        print "Mileage: " + str(self.mileage)
+        print "Tax: " + str(self.tax)
 
 car1 = Car(5000, "60mph", "Full", "15mpg")
 car2 = Car(3000, "50mph", "Not Full", "20mpg")
-car3 = Car(9000, "80mph", "Full", "25mpg")
+car3 = Car(11000, "80mph", "Full", "25mpg")
 car4 = Car(2500, "50mph", "Not Full", "20mpg")
 car5 = Car(1000, "35mph", "Not Full", "13mpg")
 car6 = Car(3000, "50mph", "Full", "30mpg")
-print car1.speed
+
+
 
